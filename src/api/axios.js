e72bba2c32b761613f256ff62aebe775bd1e13c0
S Kaushik Chopra <kaushikchopra2908@gmail.com>
2023-11-23 12:24:31 +0530

    Update backend links

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8070"
+const BASE_URL = "https://sm-order-manage.onrender.com"
 
 export default axios.create({
     baseURL: BASE_URL
